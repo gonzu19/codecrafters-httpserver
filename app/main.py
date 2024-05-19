f3ad95f009b2cdc34bd6627324ad9452b666c71e
Gonzalo <gmartipo@emeal.nttdata.com>
2024-05-19 10:28:07 +0200

    added more comments to the code

--- app/main.py
+++ app/main.py
@@ -14,16 +14,18 @@ def main():
         client_socket, client_address = server_socket.accept()
         print(f"Accepted connection from {client_address}")
 
-        # Receive the request from the client
+        # Receive the request from the client, 1024 is the maximum buffsize
         request = client_socket.recv(1024).decode('utf-8')
         print(f"Received request:\n{request}")
 
         # Prepare an HTTP response
         response = "HTTP/1.1 200 OK\r\n"
         response += "Content-Type: text/plain\r\n"
+        #The size is 13 because its the size of "HTTP/1.1 200 "
         response += "Content-Length: 13\r\n"
         response += "\r\n"
-        response += "Hello, world!"
+        body = "Hello, world!"
+        response += body
 
         # Send the response to the client
         client_socket.sendall(response.encode('utf-8'))
